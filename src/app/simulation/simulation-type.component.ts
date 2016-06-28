cc96d5d35f8fef5531dbb5b3c2363c7b21684581
ludovicladeu <lladeu@xebia.fr>
2016-06-28 13:06:43 +0200

    Simulation Component

---
+++ src/app/simulation/simulation-type.component.ts
@@ -0,0 +1,45 @@
+import {Component, Input} from "@angular/core";
+import {MessageType} from "./simulation.component";
+import {SocketService} from "../communication/socket.service";
+import {Logger} from "angular2-logger/core";
+
+@Component({
+  selector: 'simulation-type',
+  providers:[SocketService, Logger],
+  template: `
+  <form >
+    <div class="message" >
+          <label>Type : {{message.type}}</label><br/>
+          <label>Description : {{message.description}}</label><br/>
+          <textarea [(ngModel)]="message.payload"  rows="5" cols="60"></textarea>
+        <br/>
+          <button (click)="send()">send to rabbit</button>
+        <br/>
+    </div>
+    </form>
+`,
+  styles: [`
+    .message{
+       border: outset;
+       display: inline-block;
+       margin-top: 20px;
+    }
+  `]
+})
+export class SimulationTypeComponent {
+
+  @Input()
+  message:MessageType;
+
+  constructor(public socketService:SocketService, public logger:Logger){
+
+  }
+
+
+  send() {
+    let data = {type: this.message.type, payload: this.message.payload};
+    this.logger.debug(data);
+    this.socketService.pushToServer(data);
+  }
+
+}
--- src/app/simulation/simulation.component.ts
+++ src/app/simulation/simulation.component.ts
@@ -1,36 +1,33 @@
 import {Component} from  "@angular/core";
 import {SocketService} from "../communication/socket.service";
 import {Logger} from "angular2-logger/core";
+import {SimulationTypeComponent} from "./simulation-type.component";
 ;
 @Component({
   selector: 'simulation',
   providers: [SocketService, Logger],
+  directives: [SimulationTypeComponent],
   template: `
     <h1>Simulation</h1>
-    <form >
-    <label for="type">Type :</label>
-     <select [(ngModel)]="messageType" (change)="onChoiceType($event)" required  #messageControl="ngForm">
-        <option *ngFor="let type of messagesType" [value]="type" required>{{type}}</option>
-     </select>
-     <div [hidden]="!messageControl.valid">
-          <label for="payload">payload</label>
-          <textarea [(ngModel)]="payload"  rows="5" cols="60"></textarea>
-        
-        <br/>
-          <button (click)="send()">send to rabbit</button>
-        <br/>
-     </div>
-     
-    </form>`
+    <div *ngFor="let type of simulations" class="form-row">
+      <simulation-type [message]="type"></simulation-type>
+    </div>
+`
 })
 export class SimulationComponent {
 
   messagesType:Array<string> = ['VOTE_TRAIN', 'OBSTACLE_DETECTION', 'TRAIN_POSITION', 'KEYNOTE_STATE'];
   messageType:string;
   payload:string;
+  simulations:Array<MessageType>;
 
-  constructor(public socketService:SocketService,public logger:Logger) {
-
+  constructor(public socketService:SocketService, public logger:Logger) {
+    this.simulations = new Array();
+    this.simulations.push(new MessageType('VOTE_TRAIN_CHOICE', '{\\"trainId\\" : \\"<trainId>\\", \\"media\\": \\"<MOBILE | SMS |TWITTER>\\", \\"count\\": <1>}', ''));
+    this.simulations.push(new MessageType('OBSTACLE_DETECTION', '{\\"obstacle\\" : \\"true|false\\", \\"obstacleType\\": \\"<COW | RABBIT | PONEY>\\"}', ''));
+    this.simulations.push(new MessageType('OBSTACLE_CLEARED', '', ''));
+    this.simulations.push(new MessageType('TRAIN_POSITION', '{\\"trainId\\" : \\"<trainId>\\", \\"position\\": \\"<position>\\"}', ''));
+    this.simulations.push(new MessageType('KEYNOTE_STATE', '{\\"state\\": \\"<VOTE_ORDER_START | VOTE_TRAIN_END>\\"}', ''));
   }
 
   onChoiceType(event) {
@@ -53,4 +50,18 @@ export class SimulationComponent {
     this.logger.debug(data);
     this.socketService.pushToServer(data);
   }
+}
+
+export class MessageType {
+
+  public type:string;
+  public description:string;
+  public payload:string;
+
+  constructor(type:string, description:string, payload:string) {
+    this.type = type;
+    this.description = description;
+    this.payload = payload;
+  }
+
 }
