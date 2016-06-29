200e41a7b8d1b396116ce23acfc137f14f1d41c6
ludovicladeu <lladeu@xebia.fr>
2016-06-30 00:17:42 +0200

    rabbit Pub/sub Configuration

--- backend/services/rabbit-message-handler.js
+++ backend/services/rabbit-message-handler.js
@@ -1,44 +1,97 @@
 var queueName = 'xebicon';
+var exchangeName = 'xebiconExchange'
 var user = process.env.RABBIT_USER || 'xebia';
 var pwd = process.env.RABBIT_PASSWORD || 'xebia2015';
-var host = process.env.RABBIT_HOST || 'localhost';
-//var host = process.env.RABBIT_HOST || '52.28.106.238';
+// var host = process.env.RABBIT_HOST || 'localhost';
+var host = process.env.RABBIT_HOST || '52.28.106.238';
 var rabbitUrl = 'amqp://' + user + ':' + pwd + '@' + host;
 var open = require('amqplib').connect(rabbitUrl);
 var debug = require('debug')('dashboard-backend:server');
 
-module.exports = {
-  init:(io)=> {
-// Consumer
-  open.then((conn) => {
+var socketIO = null;
 
-    var ok = conn.createChannel();
-    ok = ok.then((ch) => {
-      ch.assertQueue(queueName);
+//Cosumer Logic -> Send message to from through socket
+var consumerCallback = (msg)=> {
+  if (msg !== null) {
+    debug('receive from Rabbit : ', msg.content.toString());
+    //ch.ack(msg);
+    socketIO.emit('dashboard', {content: msg.content.toString(), when: JSON.stringify(new Date())});
+  }
+};
 
-      // Consume message from rabbit queue train
-      ch.consume(queueName, (msg) => {
-        if (msg !== null) {
-          debug(msg);
-          debug(msg.content.toString());
-          ch.ack(msg);
-          io.emit('dashboard', {content: msg.content.toString(), when: JSON.stringify(new Date())});
-        }
+// Queue Consumer
+var queueConsumer = (conn) => {
+
+  var ok = conn.createChannel();
+  ok = ok.then((ch) => {
+    ch.assertQueue(queueName);
+
+    // Consume message from rabbit queue train
+    ch.consume(queueName, consumerCallback);
+  });
+
+  return ok;
+};
+
+// Publish/subscribe Consumer
+var pubSubConsumer = (conn)=> {
+  return conn.createChannel().then((ch)=>{
+    var ok = ch.assertExchange(exchangeName, 'fanout', {durable: false});
+    ok = ok.then(()=> {
+      return ch.assertQueue('', {exclusive: true});
+    });
+    ok = ok.then((qok)=>{
+      return ch.bindQueue(qok.queue, exchangeName, '').then(()=>{
+        return qok.queue;
       });
     });
+    ok = ok.then((queue)=>{
+      // return ch.consume(queue, consumerCallback, {noAck: true});
+      return ch.consume(queue, consumerCallback);
+    });
+    return ok.then(()=> {
+      console.log(' [*] Waiting for logs. To exit press CTRL+C');
+    });
+  });
+};
 
-    return ok;
-  }).then(null, console.warn);
-},
-  sendToRabbit:(data)=>{
-    open.then((conn)=>{
-      return conn.createChannel();
-    }).then((ch)=>{
-      return ch.assertQueue(queueName).then(function(ok) {
-        debug('simulation : ',data);
-        return ch.sendToQueue(queueName, new Buffer(data));
+var publishSubProducer = (data)=>{
+  return open.then((conn)=>{
+    conn.createChannel().then((ch)=> {
+      var ok = ch.assertExchange(exchangeName, 'fanout', {durable: false});
+
+      return ok.then(()=>{
+        ch.publish(exchangeName, '', new Buffer(data));
+        return ch.close();
       });
-    }).catch(console.warn);
+    });
+  }).then(null, console.warn);
+};
+
+var queueProducer = (data) => {
+  open.then((conn)=> {
+    return conn.createChannel();
+  }).then((ch)=> {
+    return ch.assertQueue(queueName).then((ok)=> {
+      debug('simulation : ', data);
+      return ch.sendToQueue(queueName, new Buffer(data));
+    });
+  }).catch(console.warn);
+};
+
+
+module.exports = {
+  init: (io)=> {
+    socketIO = io;
+    // Consumer
+    open.then(pubSubConsumer).then(null, console.warn);
+    // open.then(queueConsumer).then(null, console.warn);
+  },
+
+  sendToRabbit: (data)=> {
+    publishSubProducer(data);
+    /* uncomment if you want to use xebicon queue*/
+    //queueProducer(data);
   }
-}
+};
 
